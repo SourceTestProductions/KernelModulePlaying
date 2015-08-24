faedbef968a0cc29cdbf0255350624c10247019e
Jeremy <SourceTestFTW@gmail.com>
2015-08-24 16:33:21 -0400

    Changed the hello.c source a bit

--- helloworld/hello.c
+++ helloworld/hello.c
@@ -1,6 +1,6 @@
-#include <linux/module.h>    // included for all kernel modules
-#include <linux/kernel.h>    // included for KERN_INFO
-#include <linux/init.h>      // included for __init and __exit macros
+#include <linux/module.h>    
+#include <linux/kernel.h>    
+#include <linux/init.h>    
 
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("SourceTestProductions");
@@ -8,13 +8,13 @@ MODULE_DESCRIPTION("A Simple Hello World module");
 
 static int __init hello_init(void)
 {
-    printk(KERN_INFO "Hello world!\n");
-    return 0;    // Non-zero return means that the module couldn't be loaded.
+    printk(KERN_INFO "Hello world :D\n");
+    return 0;
 }
 
 static void __exit hello_cleanup(void)
 {
-    printk(KERN_INFO "Cleaning up module.\n");
+    printk(KERN_INFO "Exiting Module Goodbye :)\n");
 }
 
 module_init(hello_init);
